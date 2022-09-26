2803fdfc7b121d1df04977626ab531c61097752d
monosniper <vanteateam@gmail.com>
2022-09-26 15:52:05 +0500

    test

--- pages/old_middleware.js
+++ pages/old_middleware.js
@@ -1,15 +1,15 @@
-import {NextResponse} from "next/server";
-import {$routes} from "../http/routes";
-
-export default function middleware(req) {
-    const { cookies, url } = req;
-    const jwt = cookies.token
-
-    if(!url.includes($routes.login) && !url.includes($routes.register)) {
-        if(jwt === undefined) {
-            return NextResponse.rewrite(new URL($routes.login, url))
-        }
-
-        return NextResponse.next()
-    }
-}
+// import {NextResponse} from "next/server";
+// import {$routes} from "../http/routes";
+//
+// export default function middleware(req) {
+//     const { cookies, url } = req;
+//     const jwt = cookies.token
+//
+//     if(!url.includes($routes.login) && !url.includes($routes.register)) {
+//         if(jwt === undefined) {
+//             return NextResponse.rewrite(new URL($routes.login, url))
+//         }
+//
+//         return NextResponse.next()
+//     }
+// }
